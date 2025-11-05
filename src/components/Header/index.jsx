322e8d3a4ab239fd22c6760fe5be889a1789c7a5
Md Gousul Islam Hemonto <gausulislam5@gmail.com>
2025-11-05 20:12:24 +0600

    create a top header

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <header>
+      <div className="top-strip">
+        <div className="container">
+            <div className="flex items-center justify-between">
+               <div className="col1 w-[50%]">
+                <p className="text-[14px]">Get up to 50% off new season styles, limited time only</p>
+
+               </div>
+            </div>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
